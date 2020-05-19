fa823d575f4cc9a988e4cb89916f050b3074665f
Maria Zinkovskaya <my.project.git@gmail.com>
2020-05-20 01:07:11 +0300

    Added - tasks

---
+++ js/task-3.js
@@ -0,0 +1,31 @@
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+
+  getItems(items) {
+    this.item = items;
+  }
+
+  addItem(item) {
+    this.items.push(item);
+  }
+
+  removeItem(item) {}
+}
+
+const storage = new Storage([
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem('Дроид');
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Пролонгер');
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
